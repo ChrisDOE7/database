c95a0ce3813f74bb0bd37ae0c59d8a3b1ccef049
ChrisDOE <webdev@famdoe.com>
2022-11-24 15:23:15 +0100

    Update script.js
    created a new variable "pokemonList" including some objects: name, ID, height, type.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
-alert("Hello world");
-
-let favoriteFood = "Pizza";
-document.write(favoriteFood);
+let pokemonList = [
+   {
+      name: "Bulbasur",
+      ID: "#1",
+      height: 0.7,
+      type: ["Monster", "Grass"],
+   },
+   {
+      name: "Charmander",
+      ID: "#4",
+      height: 0.6,
+      type: ["Monster", "Dragon"],
+   },
+   {
+      name: "Squirtle",
+      ID: "#7",
+      height: 0.5,
+      type: ["Monster", "Water"],
+   },
+   {
+      name: "Caterpie",
+      ID: "#10",
+      height: 0.3,
+      type: "Bug",
+   },
+   {
+      name: "Weedle",
+      ID: "#13",
+      height: 0.3,
+      type: "Bug",
+   },
+];
