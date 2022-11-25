249394907e0b5164cc6250a84fa73936faa6fcfc
ChrisDOE <chris.sean.86@gmail.com>
2022-11-25 20:26:07 +0100

    js loop; design
    SCRIPT.JS
    - added a `for`-loop to filter out `height > 1` items and adds string to every match
    
    CSS
    - quick adjustments for little design improvements
    
    HTML
    - Basic <body> structure incl <header> title: "Pokedex"

--- js/script.js
+++ js/script.js
@@ -3,30 +3,52 @@ let pokemonList = [
       name: "Bulbasur",
       ID: "#1",
       height: 0.7,
-      type: ["Monster", "Grass"],
+      type: ["Monster", "Grass"]
    },
    {
       name: "Charmander",
       ID: "#4",
       height: 0.6,
-      type: ["Monster", "Dragon"],
+      type: ["Monster", "Dragon"]
+   },
+   {
+      name: "Charizard",
+      ID: "#6",
+      height: 1.7,
+      type: ["Monster", "Dragon"]
    },
    {
       name: "Squirtle",
       ID: "#7",
       height: 0.5,
-      type: ["Monster", "Water"],
+      type: ["Monster", "Water"]
    },
    {
       name: "Caterpie",
       ID: "#10",
       height: 0.3,
-      type: "Bug",
+      type: "Bug"
    },
    {
       name: "Weedle",
       ID: "#13",
       height: 0.3,
-      type: "Bug",
-   },
+      type: "Bug"
+   }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+   if (pokemonList[i].height > 1) {
+      document.write(
+         `${pokemonList[i].ID} ${pokemonList[i].name}: (height: ${pokemonList[i].height}m) Wow, that\’s big!`
+      );
+      document.write("<br>");
+   } else {
+      document.write(
+         `${pokemonList[i].ID} ${pokemonList[i].name}: (height: ${pokemonList[i].height}m)`
+      );
+      document.write("<br>");
+   }
+}
+// displays all objects including `height`-attribute and filters out the ones with attribute > 1 and adds a string to it
+// line break for better readybility
