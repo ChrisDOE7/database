d83107f81602d258292f38525f5be4ab6f9d4329
ChrisDOE <chris.sean.86@gmail.com>
2022-12-05 12:04:24 +0100

    IIFE integration to add pokemon & getAll
    pokemonRepository.add()
    - adds a new pokemon to the array
    
    pokemonRepository.getAll()
    -returns complete array

--- js/script.js
+++ js/script.js
@@ -1,54 +1,104 @@
-let pokemonList = [
-   {
-      name: "Bulbasur",
-      ID: "#1",
-      height: 0.7,
-      type: ["Monster", "Grass"]
-   },
-   {
-      name: "Charmander",
-      ID: "#4",
-      height: 0.6,
-      type: ["Monster", "Dragon"]
-   },
-   {
-      name: "Charizard",
-      ID: "#6",
-      height: 1.7,
-      type: ["Monster", "Dragon"]
-   },
-   {
-      name: "Squirtle",
-      ID: "#7",
-      height: 0.5,
-      type: ["Monster", "Water"]
-   },
-   {
-      name: "Caterpie",
-      ID: "#10",
-      height: 0.3,
-      type: "Bug"
-   },
-   {
-      name: "Weedle",
-      ID: "#13",
-      height: 0.3,
-      type: "Bug"
-   }
-];
+let pokemonRepository = (function() {
+   let pokemonList = [
+      {
+         name: "Bulbasur",
+         ID: "#1",
+         height: 0.7,
+         type: ["Monster", " Grass"]
+      },
+      {
+         name: "Charmander",
+         ID: "#4",
+         height: 0.6,
+         type: ["Monster", " Dragon"]
+      },
+      {
+         name: "Charizard",
+         ID: "#6",
+         height: 1.7,
+         type: ["Monster", " Dragon"]
+      },
+      {
+         name: "Squirtle",
+         ID: "#7",
+         height: 0.5,
+         type: ["Monster", " Water"]
+      },
+      {
+         name: "Caterpie",
+         ID: "#10",
+         height: 0.3,
+         type: "Bug"
+      },
+      {
+         name: "Weedle",
+         ID: "#13",
+         height: 0.3,
+         type: "Bug"
+      }
+   ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-   if (pokemonList[i].height > 1) {
-      document.write(
-         `${pokemonList[i].ID} ${pokemonList[i].name}: (height: ${pokemonList[i].height}m) Wow, that\’s big!`
-      );
-      document.write("<br>");
-   } else {
-      document.write(
-         `${pokemonList[i].ID} ${pokemonList[i].name}: (height: ${pokemonList[i].height}m)`
-      );
-      document.write("<br>");
-   }
-}
+   return {
+      getAll: function() {
+         return pokemonList;
+      },
+
+      add: function(pokemon) {
+         if (
+            typeof Object.keys(pokemon) ===
+            "object" /* &&
+            pokemon !== null */
+         ) {
+            pokemonList.push(pokemon);
+            console.log("Done");
+         } else {
+            return "No Pokemon, dude!";
+         }
+      }
+   };
+})();
+
+pokemonRepository.getAll().forEach(item => {
+   document.write(
+      "<p>" +
+         item.ID +
+         " " +
+         item.name +
+         ": (height:" +
+         item.height +
+         "m) Type: " +
+         item.type +
+         "</p>"
+   );
+});
+
+// function printPokemonList() {
+//    for (let i = 0; i < pokemonList.length; i++) {
+//       if (pokemonList[i].height > 1) {
+//          document.write(
+//             `<p>${pokemonList[i].ID} ${pokemonList[i].name}: (height: ${pokemonList[i].height}m) Wow, that\’s big!</p>`
+//          );
+//          document.write("<br>");
+//       } else {
+//          document.write(
+//             `<p>${pokemonList[i].ID} ${pokemonList[i].name}: (height: ${pokemonList[i].height}m)</p>`
+//          );
+//          document.write("<br>");
+//       }
+//    }
+// }
+// printPokemonList();
+
 // displays all objects including `height`-attribute and filters out the ones with attribute > 1 and adds a string to it
 // line break for better readybility
+
+/*Object.keys(pokemonList).forEach(function(_pokemon) {
+         if (pokemon.height >= 1.7) {
+            document.write(
+               _pokemon.name + _pokemon.height + " - Wow, thats big!" + "</br>"
+            );
+         } else {
+            document.write(_pokemon.name + _pokemon.height + "</br>");
+         }
+      });
+      */
